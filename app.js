7fb1287dbe236fa23e40124349692e7bb26f1b6a
TakashiHamada <hamada@gift10.net>
2024-01-27 20:22:08 +0900

    本番サーバ環境に接続

--- app.js
+++ app.js
@@ -2,28 +2,38 @@ var app = new Vue({
     el: '#app',
     data: {
         userInput: '',
-        state:'init',
+        state: 'init',
         message: '',
     },
     created: function () {
     },
     methods: {
         handleEnter() {
+            
+            this.playSe("cat");
+            
             this.state = "loading...";
             this.connectAip(this.userInput);
         },
         async connectAip(value) {
-        let url = "http://demo.cotonoha.io:8000/homekotoba";
-        
-        const response = await axios.get(url, {
-            params: {
-                character: "goblin",
-                message: value,
-            }
-        });
-        
-        this.state = "finish";
-        this.message = response.data;
+            let url = "http://demo.cotonoha.io:8000/homekotoba";
+
+            const response = await axios.get(url, {
+                params: {
+                    character: "goblin",
+                    message: value,
+                }
+            });
+
+            this.state = "finish";
+            this.message = response.data;
+        },
+        playSe(fileName) {
+            var sound = new Howl({
+                src: ['se/' + fileName + '.mp3']
+            });
+
+            sound.play();
         }
     }
 })
